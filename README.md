dab3dcbb87309e4af9bdb499aed6886c9b2622c4
jinyong1015 <jjyim1015@gmail.com>
2023-07-18 04:08:15 +0900

    reactNodejs-study

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
 
-# 1. Node JS, EXPRESS 다운 
-# 2. MONGO DB 연결
-# 3. BodyParser& PostMan & 회원가입 기능 추가
-# 4. Nodemon 설치
+# reactNodejs-study
+----
+1. Node JS, EXPRESS 다운 
+2. MONGO DB 연결
+    - 5.0 이상 version 부터 callback 함수 지원 x 
+3. BodyParser& PostMan & 회원가입 기능 추가
+4. Nodemon 설치
+5. mongoDB 비밀정보 보호 관리 기능 추가
+6. Bcrypt로 비밀번호 암호화 하기 (보안성 강화)
+7. Bcrypt로 로그인 기능 추가
+8. jsonwebtoke으로 토큰 생성
+9. Auth 기능 추가
+10. 로그아웃 기능 추가
+
+
+
+# 힘들었던 점 
+logout에는 성공했지만 mongodb에서 token이 사라지지 않아 해결하기 까지 몇 시간을 계속 붙잡고 있었다 . . . 
+실제 localhost에서는 브라우저에서 자동으로 x_auth를 받아오지 못하므로  req.cookies.x_auth로 직접 넣어줘야 함
+----
+local host코드
+---
+app.get("/logout", auth, (req, res) => {
+  //req.user를 미들웨어에서 가져올 수 있다.
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+        return res.status(200).send({ success: true });
+     });
+  });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name : {
@@ -32,6 +35,89 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save',function(next){
+  var user = this;
+
+  if(user.isModified('password')) {
+
+  // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err)
+        user.password=hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  var user = this;
+  console.log("사용자 : " + user)
+  console.log("comparepassword(평문)   :" + plainPassword);
+  console.log("comparepassword(암호문) :" + user.password);
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    console.log("plainpassword(평문) :" +plainPassword);
+    console.log("this.password(복호화) :" + this.password);
+    console.log("isMatch : " +isMatch);
+    if(err) return cb(err);
+    cb(null, isMatch) 
+  })
+}
+
+
+
+
+
+userSchema.methods.generateToken = function(cb) {
+
+  var user = this;
+  // jsonwebtoken을 이용해서 token을 생성하기
+  
+  let token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+  user.token = token;
+
+  user.save().then(() => {
+    cb(null, user);
+  }).catch((err) => {
+    return cb(err);
+  })
+
+
+  // user.save(function (err, user) {
+  //   if (err) return cb(err);
+  //   cb(null, user);
+  // });
+  
+}
+
+userSchema.statics.findByToken = function( token , cb) {
+  var user = this;
+
+  // 토큰을 decode
+  jwt.verify(token, 'secretToken', function(err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+    // user.findOne({"_id" : decoded, "token":token}, function(err, user) {
+
+    //   if(err) return cb(err);
+    //   cb(null,user)
+    // })
+
+    user.findOne({"_id" : decoded, "token":token}).then(() => { 
+      cb(null,user);
+  }).catch((err) => {
+    return cb(err);
+  })
+})
+}
 
 const User = mongoose.model('User', userSchema)
 
