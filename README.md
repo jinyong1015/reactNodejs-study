a57c0acffc474ac9fb73e0d2ab0d482bf333c953
jinyong1015 <jjyim1015@gmail.com>
2023-07-17 14:35:39 +0900

    mongoDB 비밀정보 보호 관리 기능 추가

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# 1. Node JS, EXPRESS 다운 
+# 2. MONGO DB 연결
+# 3. BodyParser& PostMan & 회원가입 기능 추가
+# 4. Nodemon 설치
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form/urlencoded 분석
+app.use(bodyParser.json()); // application/json 분석
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jinyong:wlsdyd05@cluster0.b06hcjj.mongodb.net/?retryWrites=true&w=majority', {})
+mongoose.connect(config.mongoURI, {})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
@@ -12,8 +18,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/register', (req,res) => {
+  // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save().then(() => {
+      res.status(200).json({success:true})
+
+    }). catch((err) => {
+      return res.json({success:false,err})
+    });
+    
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-// mongodb 연결
